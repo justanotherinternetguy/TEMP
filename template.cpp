29b44d21aa74dcdd24491e0e153f1d33a688100b
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-04-25 14:50:34 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define PI 3.1415926535897932384626433832795
+#define INF (int)1e9
+typedef long long ll;
+
+string solve () {
+    return "test";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    
+    int rounds;
+    scanf("%d", &rounds);
+    
+    for (int i = 1; i <= rounds; i++) {
+        cout << "Case #" << i << ": " << solve() << "\n";
+    }
+    
+    return 0;    
+}
